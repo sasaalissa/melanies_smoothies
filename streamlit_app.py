51f1111309ee1661cc2e2d30fc3cf32a198b92e6
sasaalissa <56672501+sasaalissa@users.noreply.github.com>
2025-11-19 09:23:25 +0100

    Fix method name case for to_pandas conversion

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 #Convert the Snowpark Dataframe to Pandas Dataframe so we can use the LOC function
-pd_df = my_dataframe.to_Pandas()
+pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
 
